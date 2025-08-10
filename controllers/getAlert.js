4e5b7f77ef028f151cdeeec13e9dd8263accaf18
Israel <olaoyeaisrael@gmail.com>
2025-08-10 10:00:43 +0100

    notification update

--- controllers/getAlert.js
+++ controllers/getAlert.js
@@ -1,12 +1,21 @@
 const Alert = require('../models/Alert');
+const Bed = require('../models/BedC');
 
 const getAlertsForCaregiver = async (req, res) => {
   try {
     const { caregiverId } = req.params;
 
-    const alerts = await Alert.find({ caregiverId })
+    const beds = await Bed.find({ caregiverId });
+
+    const bedIds = beds.map(bed => bed._id);
+
+    // const alerts = await Alert.find({ caregiverId })
+    //   .sort({ timestamp: -1 })
+    //   .limit(20);
+     const alerts = await Alert.find({ bedId: { $in: bedIds } })
       .sort({ timestamp: -1 })
-      .limit(20);
+      .limit(20)  // Optional: limit to the latest 20 alerts
+      .populate('bedId');
 
     res.status(200).json(alerts);
   } catch (err) {
